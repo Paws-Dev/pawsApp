8dabcfa94d9a874cab4e5b4cbd155e1c702887d7
a.groshev <a.groshev@i-teco.ru>
2025-02-27 14:47:23 +0300

    errs modify

--- errs/Errs.go
+++ errs/Errs.go
@@ -2,82 +2,15 @@ package errs
 
 import (
 	"errors"
-	"strings"
 )
 
-type Error struct {
-	error   error
-	handler func(error) error
-}
-
-func New(message string) *Error {
-	return &Error{error: errors.New(message)}
-
-}
-
-func FromError(err error) *Error {
-	if err == nil {
-		return &Error{}
+func GetRoot(err error) error {
+	for err != nil {
+		unwrapped := errors.Unwrap(err)
+		if unwrapped == nil {
+			return err
+		}
+		err = unwrapped
 	}
-	return &Error{
-		error: err,
-	}
-}
-
-func (e *Error) Root() string {
-	if e == nil || e.error == nil {
-		return ""
-	}
-	trace := strings.Split(e.error.Error(), "\n")
-	if len(trace) == 0 {
-		return ""
-	}
-	return trace[len(trace)-1]
-}
-
-func (e *Error) ErrorF(symbol string) string {
-	if e == nil || e.error == nil {
-		return ""
-	}
-	replacer := strings.NewReplacer(
-		"\r\n", symbol,
-		"\n", symbol,
-	)
-	return replacer.Replace(e.error.Error())
-}
-
-func (e *Error) Error() string {
-	return e.error.Error()
-}
-
-func (e *Error) Add(err error) *Error {
-	if e == nil {
-		return nil
-	}
-
-	if err == nil {
-		return e
-	}
-
-	if e.error == nil {
-		e.error = err
-	} else {
-		e.error = errors.Join(e.error, err)
-	}
-
-	return e
-}
-
-func (e *Error) Is(target error) bool {
-	if e == nil || e.error == nil {
-		return false
-	}
-	return errors.Is(e.error, target)
-}
-
-func (e *Error) As(target interface{}) bool {
-	if e == nil || e.error == nil {
-		return false
-	}
-	return errors.As(e.error, &target)
+	return nil
 }
